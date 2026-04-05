ac080c4d99ab09a54b4191ce60f850f0b7add9a9
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-04-05 16:58:45 +0100

    linkedlist basic first impl

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+type  Bucket struct{
+	bucketIndex int
+	entries *LinkedList
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+type Node struct {
+	value string
+	next  *Node
+}
+
 func fnv_a1(s string) uint64 {
 	var h uint64 = 14695981039346656037
 	for i := 0; i < len(s); i++ {
@@ -15,7 +29,41 @@ func bucketIndex(s string, numBuckets int) int {
 	return int(hash % uint64(numBuckets))
 }
 
+func NewBucket(bucketIndex int) *Bucket {
+	return &Bucket{
+		bucketIndex: bucketIndex,
+		entries: NewLinkedList(),
+	}
+}
+
+func NewLinkedList() *LinkedList {
+	return &LinkedList{head: nil}
+}
+
+func (ll *LinkedList) Append(value string) {
+	newNode := &Node{value: value, next: nil}
+	if ll.head==nil{
+		ll.head=newNode
+		return
+	}
+	current := ll.head
+	for current.next!=nil{
+		current=current.next
+	}
+	current.next=newNode
+}
+
 func main() {
+	//test LinkedList
+	ll := NewLinkedList()
+	ll.Append("first")
+	ll.Append("second")
+	ll.Append("third")
+	current := ll.head
+	for current != nil {
+		print(current.value, " ")
+		current = current.next
+	}
 	// Example usage
 	str := "aleluia"
 	numBuckets := 30
